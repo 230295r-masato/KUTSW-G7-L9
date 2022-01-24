403b8a4577a1f0a99ea515e88047844281078f99
230295r-masato <72247111+230295r-masato@users.noreply.github.com>
2022-01-24 12:50:29 +0900

    Update connectDB.php

--- DBmodule/connectDB.php
+++ DBmodule/connectDB.php
@@ -1,7 +1,8 @@
 <?php
 // データベースに接続するために必要なデータソースを変数に格納
 // mysql:host=ホスト名;dbname=データベース名;charset=文字エンコード
-   $dsn = 'mysql:host=192.168.10.34;dbname=sampledb;charset=utf8';
+//   $dsn = 'mysql:host=192.168.10.34;dbname=sampledb;charset=utf8';
+   $dsn = 'mysql:host=222.229.69.200;dbname=sampledb;charset=utf8';
 
    // データベースのユーザー名
    $user = 'admin';
