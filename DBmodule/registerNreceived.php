00f92f0d334fcb2e6112a02ce12d360d2158d88d
230295r-masato <72247111+230295r-masato@users.noreply.github.com>
2022-01-27 18:24:39 +0900

    Add files via upload

--- DBmodule/registerNreceived.php
+++ DBmodule/registerNreceived.php
@@ -2,21 +2,21 @@
 
 require("connectDB.php");
 
-//$json = file_get_contents('php://input');
-//$array = json_decode($json);
-//$reserve_id = intval($array->text1);
-//$user_id = intval($array->text2);
-//$reserve_time = $array->text3;
-//$start_lat = $array->text4;
-//$start_lng = $array->text5;
-//$goal_lat = $array->text6;
-//$goal_lng = $array->text7;
+$json = file_get_contents('php://input');
+$array = json_decode($json);
+$user_id = $array->text1);
+$reserve_time = $array->text2;
+$start_lat = $array->text3;
+$start_lng = $array->text4;
+$goal_lat = $array->text5;
+$goal_lng = $array->text6;
+
 
 try{
     $dbh = new PDO($dsn, $user, $password);
-    $sql = "INSERT INTO NRECEIVED VALUES(:reserve_id, :user_id, :reserve_time, :start_lat, :start_lng, :goal_lat, :goal_lng)";
+    $sql = "INSERT INTO NRECEIVED (USER_ID, RESERVE_TIME, START_LAT, START_LNG, GOAL_LAT, GOAL_LNG) VALUES(:user_id, :reserve_time, :start_lat, :start_lng, :goal_lat, :goal_lng)";
     $stmt = $dbh->prepare($sql);
-    $params = array(':reserve_id'=>$reserve_id, ':user_id'=>$user_id, ':reserve_time'=>$reserve_time, ':start_lat'=>$start_lat, ':start_lng'=>$start_lng, ':goal_lat'=>$goal_lat, ':goal_lng'=>$goal_lng);
+    $params = array(':user_id'=>$user_id, ':reserve_time'=>$reserve_time, ':start_lat'=>$start_lat, ':start_lng'=>$start_lng, ':goal_lat'=>$goal_lat, ':goal_lng'=>$goal_lng);
     $stmt->execute($params);
     
  }catch(PDOException $e){
